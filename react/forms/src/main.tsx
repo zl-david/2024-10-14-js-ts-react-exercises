77c28f0d985c359b7fe867fb49434cf8cab7d8a8
david <david.vangompel@gmail.com>
2024-10-11 18:15:38 +0200

    forms exercise

--- react/forms/src/main.tsx
+++ react/forms/src/main.tsx
@@ -2,13 +2,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { ThemeProvider } from "@/ui/ThemeProvider.tsx";
-import SimpleForm from "./components/SimpleForm";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* <ThemeProvider defaultTheme="light" storageKey="ui-theme"> */}
     <div>{"Hello World"}</div>
-    <SimpleForm />
     {/* </ThemeProvider> */}
   </React.StrictMode>
 );
